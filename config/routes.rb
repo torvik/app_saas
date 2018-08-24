1889215037316328ba42c7aae0301b0b7ec7a1f2
torvik <pless.victor@gmail.com>
2018-08-24 16:56:07 -0300

    pg 100

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/update'
+
   devise_for :users, controllers: { registrations: "registrations"}
   resource :accounts 
 
